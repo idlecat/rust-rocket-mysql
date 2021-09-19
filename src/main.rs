3c933213f5b5520ff150f41c9ab331737b76adad
Xiaohu Dong <xdong@Xiaohus-MBP.local>
2021-09-19 16:13:19 +0800

    Add rocket_sync_db_pools to rust code and compiles

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+#[macro_use]
+extern crate diesel;
+
+use rocket_sync_db_pools::{database, diesel as rocket_diesel};
+
+#[database("mysql_db")]
+struct MySqlConn(rocket_diesel::MysqlConnection);
 
 #[get("/")]
 fn index() -> &'static str {
@@ -7,6 +16,7 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-        rocket::build().mount("/", routes![index])
+        rocket::build()
+                .attach(MySqlConn::fairing())
+                .mount("/", routes![index])
 }
-
