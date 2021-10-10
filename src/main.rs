692ca4a9839f926b1d87416338e92fecf7514bb6
Xiaohu Dong <xdong@Xiaohus-MBP.lan>
2021-10-10 12:32:54 -0700

    About to create post

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,14 @@ use diesel::prelude::*;
 use models::*;
 use schema::posts::dsl::*;
 
+use rocket::response::Debug;
 use rocket::serde::json::Json;
 use rocket_sync_db_pools::{database, diesel as rocket_diesel};
 
 #[database("mysql_db")]
-struct MySqlConn(rocket_diesel::MysqlConnection);
+struct Db(rocket_diesel::MysqlConnection);
+
+type Result<T, E = Debug<diesel::result::Error>> = std::result::Result<T, E>;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -23,16 +26,29 @@ fn index() -> &'static str {
 }
 
 #[get("/post/<post_id>")]
-async fn get_post(conn: MySqlConn, post_id: i64) -> Option<Json<Post>> {
-        conn.run(move |c| posts.filter(id.eq(post_id)).first(c))
+async fn get_post(db: Db, post_id: i64) -> Option<Json<Post>> {
+        db.run(move |c| posts.filter(id.eq(post_id)).first(c))
                 .await
                 .map(Json)
                 .ok()
 }
 
+#[post("/post/create", data = "<post>")]
+async fn create_post(db: Db, post: Json<NewPost>) -> Json<Post> {
+        let insert_rows = db
+                .run(move |conn| diesel::insert_into(posts).values(&*post).execute(conn))
+                .await
+                .ok()
+                .unwrap();
+        Json(Post {
+                id: insert_rows as i64,
+                content: "good".to_string(),
+        })
+}
+
 #[launch]
 fn rocket() -> _ {
         rocket::build()
-                .attach(MySqlConn::fairing())
-                .mount("/", routes![index, get_post])
+                .attach(Db::fairing())
+                .mount("/", routes![index, get_post, create_post])
 }
--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,15 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 use crate::schema::posts;
 
 #[derive(Debug, Clone, Queryable, Insertable, Serialize)]
 pub struct Post {
   pub id: i64,
+  pub content: String,
+}
+
+#[derive(Insertable, Deserialize)]
+#[table_name = "posts"]
+pub struct NewPost {
   pub content: String,
 }
